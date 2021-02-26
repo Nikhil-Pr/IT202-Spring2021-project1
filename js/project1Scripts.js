bec1aefea0436606d85daec8df78c3f3b5232099
Nikhil Prasad <nprasad493@gmail.com>
2021-02-26 17:23:44 -0600

    Click handling for navbar links (Data, home and filter so far)

--- js/project1Scripts.js
+++ js/project1Scripts.js
@@ -1,11 +1,45 @@
 let navigationOptions = document.querySelectorAll(".navOpt")
-
+let targetView = document.querySelector("#home")
 window.onload = () => {
-    let onLoadDisplay = document.querySelector("#home");
-    onLoadDisplay.style.display = "block";
+    targetView.style.display = "block";
 }
 
+navigationOptions.forEach((btn) => {
 
+    btn.addEventListener('click', function (event) {
+        document.querySelectorAll(".screen").forEach( (screen) => {
+            screen.style.display="none";
+        })
+        switch (event.target.getAttribute('data-screens')) {
+            case "home":
+                loadHome()
+                break
+            case "filter":
+                loadFilter()
+                break
+            case "data":
+                loadData()
+                break
+        }
+
+    })
+})
+
+
+function loadHome() {
+    targetView = document.querySelector("#home")
+    targetView.style.display = "block";
+}
+
+function loadFilter() {
+    targetView = document.querySelector("#filter")
+    targetView.style.display = "block";
+}
+
+function loadData(){
+    targetView = document.querySelector("#data")
+    targetView.style.display = "block";
+}
 
 
 
