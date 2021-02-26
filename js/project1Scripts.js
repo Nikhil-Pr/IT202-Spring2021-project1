1a7f131ded64d2d0dfab65cb2537817af0dad2dc
Nikhil Prasad <nprasad493@gmail.com>
2021-02-26 16:35:14 -0600

    Home/Instructions commit

---
+++ js/project1Scripts.js
@@ -0,0 +1,11 @@
+let navigationOptions = document.querySelectorAll(".navOpt")
+
+window.onload = () => {
+    let onLoadDisplay = document.querySelector("#home");
+    onLoadDisplay.style.display = "block";
+}
+
+
+
+
+
