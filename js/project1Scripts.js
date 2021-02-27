f32d3c534cc60c543925b5c13ecde8ac5c657b64
Nikhil Prasad <nprasad493@gmail.com>
2021-02-27 14:55:47 -0600

    Filter form adjustments, Data screen design and filter handling

--- js/project1Scripts.js
+++ js/project1Scripts.js
@@ -1,5 +1,6 @@
 let navigationOptions = document.querySelectorAll(".navOpt")
 let targetView = document.querySelector("#home")
+let fetchRequest = "https://data.cityofchicago.org/resource/aksk-kvfp.json?"
 window.onload = () => {
     targetView.style.display = "block";
 }
@@ -24,6 +25,37 @@ navigationOptions.forEach((btn) => {
 
     })
 })
+
+function handleFilter(){
+    let filterName = document.querySelector("#filterName")
+    let filterPin = document.querySelector("#filterPin")
+    let filterAddress = document.querySelector("#filterAddress")
+    let filterSqft = document.querySelector("#filterSqft")
+    let filterNumber = document.querySelector("#filterNumber")
+    let filterZIP = document.querySelector("#filterZIP")
+    fetchRequest = "https://data.cityofchicago.org/resource/aksk-kvfp.json?"
+    if(filterName.value !== ""){
+        fetchRequest += `&community_area_name=${filterName.value}`
+    }
+    if(filterPin.value !== ""){
+        fetchRequest += `&pin=${filterName.value}`
+    }
+    if(filterAddress.value !== ""){
+        fetchRequest += `&address=${filterAddress.value}`
+    }
+    if(filterSqft.value !== ""){
+        fetchRequest += `&sq_ft=${filterSqft.value}`
+    }
+    if(filterNumber.value !== ""){
+        fetchRequest += `&community_area_number=${filterNumber.value}`
+    }
+    if(filterZIP.value !== ""){
+        fetchRequest += `&zip_code=${filterZIP.value}`
+    }
+
+    console.log(fetchRequest)
+
+}
 
 
 function loadHome() {
