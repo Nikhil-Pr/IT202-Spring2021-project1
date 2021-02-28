f4902060edf32362b3eb12ece3c1e3f2437f8f72
Nikhil Prasad <nprasad493@gmail.com>
2021-02-27 21:14:57 -0600

    Updated about page, Mapped data functionality and implementation

--- js/project1Scripts.js
+++ js/project1Scripts.js
@@ -8,8 +8,8 @@ window.onload = () => {
 navigationOptions.forEach((btn) => {
 
     btn.addEventListener('click', function (event) {
-        document.querySelectorAll(".screen").forEach( (screen) => {
-            screen.style.display="none";
+        document.querySelectorAll(".screen").forEach((screen) => {
+            screen.style.display = "none";
         })
         switch (event.target.getAttribute('data-screens')) {
             case "home":
@@ -21,6 +21,9 @@ navigationOptions.forEach((btn) => {
             case "data":
                 loadData()
                 break
+            case "map":
+                loadMap()
+                break
             case "about":
                 loadAbout()
                 break
@@ -29,7 +32,7 @@ navigationOptions.forEach((btn) => {
     })
 })
 
-function handleFilter(){
+function handleFilter() {
     let filterName = document.querySelector("#filterName")
     let filterPin = document.querySelector("#filterPin")
     let filterAddress = document.querySelector("#filterAddress")
@@ -37,22 +40,22 @@ function handleFilter(){
     let filterNumber = document.querySelector("#filterNumber")
     let filterZIP = document.querySelector("#filterZIP")
     fetchRequest = "https://data.cityofchicago.org/resource/aksk-kvfp.json?"
-    if(filterName.value !== ""){
+    if (filterName.value !== "") {
         fetchRequest += `&community_area_name=${filterName.value}`
     }
-    if(filterPin.value !== ""){
+    if (filterPin.value !== "") {
         fetchRequest += `&pin=${filterPin.value}`
     }
-    if(filterAddress.value !== ""){
+    if (filterAddress.value !== "") {
         fetchRequest += `&address=${filterAddress.value}`
     }
-    if(filterSqft.value !== ""){
+    if (filterSqft.value !== "") {
         fetchRequest += `&sq_ft=${filterSqft.value}`
     }
-    if(filterNumber.value !== ""){
+    if (filterNumber.value !== "") {
         fetchRequest += `&community_area_number=${filterNumber.value}`
     }
-    if(filterZIP.value !== ""){
+    if (filterZIP.value !== "") {
         fetchRequest += `&zip_code=${filterZIP.value}`
     }
     console.log(fetchRequest)
@@ -69,7 +72,7 @@ function loadFilter() {
     targetView.style.display = "block";
 }
 
-function loadData(){
+function loadData() {
     console.log(fetchRequest)
     targetView = document.querySelector("#data")
     targetView.style.display = "block";
@@ -80,14 +83,48 @@ function loadData(){
         .then(data => {
             data.forEach(row => {
                 let cardCloneLoop = cardClone.cloneNode(true)
-                cardCloneLoop.querySelector('.card-title').innerHTML = `${row.community_area_name} - ${row.pin}`
-                cardCloneLoop.querySelector('.card-text').innerHTML = `${row.address} - ${row.sq_ft}`
+                cardCloneLoop.querySelector('.card-title').innerHTML = `${row.community_area_name ?? "No Community Area Name Specified"} <br /> ${row.pin ?? "No Pin Specified"}`
+                cardCloneLoop.querySelector('.card-text').innerHTML = `Address: ${row.address ?? "No Address Specified"} <br /> Sqft: ${row.sq_ft ?? "No Sqft Specified"}`
                 document.querySelector('#cards').appendChild(cardCloneLoop)
             })
         })
 }
 
-function loadAbout(){
+function loadMap() {
+    console.log("loadmap")
+    targetView = document.querySelector("#mapScreen")
+    targetView.style.display = "block";
+    const chicagoLatLng = {lat: 41.8781, lng: -87.6298}
+    const dataMap = new google.maps.Map(document.getElementById("map"),{
+        zoom:11,
+        center:chicagoLatLng
+    })
+    fetch(fetchRequest)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(row => {
+                let marker = new google.maps.Marker({
+                    position: {lat: parseFloat(row.latitude), lng: parseFloat(row.longitude)},
+                    map:dataMap
+                })
+                let infoWindow = new google.maps.InfoWindow({
+                    position:{lat: parseFloat(row.latitude), lng:parseFloat(row.longitude)},
+                    content:
+                    `
+                    <h4>${row.community_area_name ?? "No Community Area Name Specified"}</h4>
+                    <h6>Pin: ${row.pin ?? "No Pin Specified"}</h6>
+                    <h5>Address: ${row.address ?? "No Address Specified"} </h5>
+                    <h6>Sqft:${row.sq_ft ?? "No Sqft Specified"}</h6>
+                    `
+                })
+                marker.addListener("mouseover", () => infoWindow.open(dataMap,marker))
+                marker.addListener("mouseout", () => infoWindow.close(dataMap, marker))
+            })
+        })
+
+}
+
+function loadAbout() {
     targetView = document.querySelector("#about")
     targetView.style.display = "block";
 }
